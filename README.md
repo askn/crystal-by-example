1ed691304718d8bda25f0d36374daa1c74421053
Aşkın Gedik <askn@bil.omu.edu.tr>
2015-11-03 16:57:28 +0200

    if else

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 * [Hello](hello/hello.cr)
 * [Values](values/values.cr)
 * [Variables](variables/variables.cr)
+* [While](while/while.cr)
+* [If-else](if-else/if-else.cr)
---
+++ if-else/if-else.cr
@@ -0,0 +1,17 @@
+if 7 % 2 == 0
+  puts "7 is even"
+else
+  puts "7 is odd"
+end
+
+if 8 % 4 == 0
+  puts "8 is divisible by 4"
+end
+
+if (num = 9) < 0
+  puts "#{num}, is negative"
+elsif num < 10
+  puts "#{num}, has 1 digit"
+else
+  puts "#{num}, has multiple digits"
+end

a5763cdf2b5d204d5dee91306307c3c7dafcbd97
Jonathan <greenbigfrog@gmail.com>
2017-12-20 17:18:23 +0100

    Update argv.cr

--- command-line-args/argv.cr
+++ command-line-args/argv.cr
@@ -1,6 +1,8 @@
 # Demo of command line arguments
 # ARGV[0]: First command line argument
 # (not the executable name)
+# (Be aware: `crystal build` sends all arguments to the compiler.
+# Add an additional `--` between compiler arguments and the other)
 # ARGV is an array of strings
 puts "Number of command line arguments: #{ARGV.size}"
 ARGV.each_with_index {|arg, i| puts "Argument #{i}: #{arg}"}

8f21701cea7209061ac3f1b29ddbf366ee42f9a7
Jack Thorne <jack@kilmer.co>
2019-02-06 18:46:50 -0600

    update time spec

--- time/time.cr
+++ time/time.cr
@@ -1,5 +1,5 @@
 puts Time.now
-t = Time.new(2002, 10, 31, 2, 2, 2, 999, Time::Kind::Local)
+t = Time.new(2002, 10, 31, 2, 2, 2)
 p t
 puts t.year
 puts t.month
@@ -7,5 +7,4 @@ puts t.day
 puts t.hour
 puts t.minute
 puts t.second
-puts t.millisecond
 puts t.day_of_week

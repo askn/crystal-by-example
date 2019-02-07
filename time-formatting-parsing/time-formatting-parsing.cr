c22e6e7020c772f6ff3cf75280eee2c2f047adaa
Jack <javajax@gmail.com>
2019-02-07 00:20:20 -0600

    update time parsing (#21)

--- time-formatting-parsing/time-formatting-parsing.cr
+++ time-formatting-parsing/time-formatting-parsing.cr
@@ -1,5 +1,5 @@
-puts Time.parse("2012-11-01 22:08:12", "%F %T")
+puts Time.parse_utc("2012-11-01 22:08:12", "%F %T")
 
-puts Time.parse("Fri Oct 31 23:00:24 2014", "%c")
+puts Time.parse_utc("Fri Oct 31 23:00:24 2014", "%c")
 
-puts Time.parse("20150624", "%Y%m%d")
+puts Time.parse_utc("20150624", "%Y%m%d")

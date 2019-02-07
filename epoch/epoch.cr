b23623b52dc6c2c2de1014e499d74c110623a44d
Jack <javajax@gmail.com>
2019-02-07 00:20:33 -0600

    update epoch to unix time (#20)

--- epoch/epoch.cr
+++ epoch/epoch.cr
@@ -1,5 +1,5 @@
-puts Time.epoch(628232400)
+puts Time.unix(628232400)
 t = Time.new(1989, 11, 28, 5, 0, 0)
-puts t.epoch
-puts t.epoch_ms
+puts t.to_unix
+puts t.to_unix_ms
 #  1989-11-28 05:00:00 UTC
